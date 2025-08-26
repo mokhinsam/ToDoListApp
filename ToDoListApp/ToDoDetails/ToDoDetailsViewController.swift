a88594946c870819cce51e60d2ea009ab030be3f
Darina Samokhina <mokhinsam@gmail.com>
2025-08-26 18:54:30 +0300

    Added ToDoDetailsViewController.Added outlets: title and body textview+heighConst, dateLabel, scrollView.

---
+++ ToDoListApp/ToDoDetails/ToDoDetailsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ToDoDetailsViewController.swift
+//  ToDoListApp
+//
+//  Created by Дарина Самохина on 26.08.2025.
+//
+
+import UIKit
+
+class ToDoDetailsViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var titleTextView: UITextView!
+    @IBOutlet weak var titleTextViewHeight: NSLayoutConstraint!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var bodyTextView: UITextView!
+    @IBOutlet weak var bodyTextViewHeight: NSLayoutConstraint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+
+}
