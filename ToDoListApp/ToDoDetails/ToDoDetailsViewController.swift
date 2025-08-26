f6295fa09d3a08159f7d2d4992308bd87110bca8
Darina Samokhina <mokhinsam@gmail.com>
2025-08-26 19:20:20 +0300

    Added in TDDetailsVC: titleTextView.becomeFirstResponder(), keyboard settings, scrollView settings and adaptive height settings for textViews. Changed bodyTextView height to 250.

--- ToDoListApp/ToDoDetails/ToDoDetailsViewController.swift
+++ ToDoListApp/ToDoDetails/ToDoDetailsViewController.swift
@@ -18,11 +18,81 @@ class ToDoDetailsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        titleTextView.delegate = self
+        bodyTextView.delegate = self
+        titleTextView.isScrollEnabled = false
+        bodyTextView.isScrollEnabled = false
         
+        configureUI()
+        registerForKeyboardNotifications()
     }
     
 
 
+    deinit {
+        unregisterForKeyboardNotifications()
+    }
+}
 
+//MARK: - Private Methods
+extension ToDoDetailsViewController {
+    private func configureUI() {
+        titleTextView.becomeFirstResponder()
+    }
+    
+    private func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow(_:)),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide(_:)),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+
+    private func unregisterForKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self)
+    }
+
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        guard let userInfo = notification.userInfo else { return }
+        let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey]
+        guard let keyboardFrame = keyboardFrame as? CGRect else { return }
+        let keyboardHeight = keyboardFrame.height
+        scrollView.contentInset.bottom = keyboardHeight
+        scrollView.verticalScrollIndicatorInsets.bottom = keyboardHeight
+    }
+
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        scrollView.contentInset.bottom = 0
+        scrollView.verticalScrollIndicatorInsets.bottom = 0
+    }
+    
+}
+
+//MARK: - UITextViewDelegate
+extension ToDoDetailsViewController: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        updateTextViewHeights()
+    }
+
+    func updateTextViewHeights() {
+        let maxWidth = titleTextView.frame.width
+        let titleSize = titleTextView.sizeThatFits(
+            CGSize(width: maxWidth, height: .greatestFiniteMagnitude)
+        )
+        titleTextViewHeight.constant = max(titleSize.height, 50)
+        
+        let bodySize = bodyTextView.sizeThatFits(
+            CGSize(width: maxWidth, height: .greatestFiniteMagnitude)
+        )
+        bodyTextViewHeight.constant = max(bodySize.height, 250)
+
+        view.layoutIfNeeded()
+    }
 }
