e06435fb219a071cd59a5cbc8663aa878f05e541
Darina Samokhina <mokhinsam@gmail.com>
2025-08-26 18:44:04 +0300

    Added segue id fromToDoListToToDoDetails. Added yelloy color for navBar items, chaneged back button name. Changed dateLabel, added setings to textViews in TDDetailsVC.

---
+++ ToDoListApp/ToDoList/ToDoListViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ToDoListViewController.swift
+//  ToDoListApp
+//
+//  Created by Дарина Самохина on 26.08.2025.
+//
+
+import UIKit
+
+class ToDoListViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var toDoCountLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+
+    @IBAction func addButtonDidPressed(_ sender: UIButton) {
+        performSegue(withIdentifier: "fromToDoListToToDoDetails", sender: self)
+    }
+    
+}
+
+//MARK: - Private Methods
+extension ToDoListViewController {
+    private func configureUI() {
+        navigationController?.navigationBar.tintColor = UIColor.systemYellow
+    }
+}
+
+
+
